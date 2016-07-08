09140e0487ea810ecf18315e6764a7f3363522e7
chanywa <cbible.kim@samsung.com>
2016-07-08 21:25:21 +0900

    fixed to calcurate minimal hit area of the marker
    Change-Id: I2ba61fff95e6dd2050168ec02a12d20b94ea0cc2

--- src/maps_util.cpp
+++ src/maps_util.cpp
@@ -17,6 +17,9 @@
 #include "maps_util.h"
 #include "maps_error.h"
 #include <glib.h>
+#include <system_info.h>
+
+#define DEFAULT_UNKNOWN_DPI 132
 
 int maps_set_string(const char *src, const int max_length, char **dst)
 {
@@ -34,4 +37,11 @@ int maps_get_string(const char *src, const int max_length, char **dst)
 		return MAPS_ERROR_INVALID_PARAMETER;
 	*dst = g_strndup(src, max_length);
 	return MAPS_ERROR_NONE;
+}
+
+int maps_get_display_dpi()
+{
+    int dpi = DEFAULT_UNKNOWN_DPI;
+    system_info_get_platform_int("http://tizen.org/feature/screen.dpi", &dpi);
+    return dpi;
 }
